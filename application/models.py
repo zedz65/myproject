8731b4836d695072236011a823ae77b3aeccac37
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-19 16:16:55 +0000

    Update models.py

--- application/models.py
+++ application/models.py
@@ -4,7 +4,7 @@ class Car(db.Model):
     car_id = db.Column(db.Integer, primary_key=True)
     make = db.Column(db.String(30), nullable=False)
     model = db.Column(db.String(30), nullable=False)
-    year = db.Column(db.Integer(4), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
     reg = db.Column(db.String(7), nullable=False, unique=True)
 
     def __repr__(self):
