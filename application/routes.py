3a9bf9befa94a564b4fe0acc4441f8ff2418a28a
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-19 16:43:40 +0000

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -2,11 +2,14 @@
 from flask import render_template
 # import the app object from the ./application/__init__.py
 from application import app
+from application.models import Car
+
 # define routes for / & /home, this function will be called when these are accessed
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home')
+ carData = Car.query.first()
+ return render_template('home.html', title='Home', car=carData)
 
 @app.route('/car')
 def car():
