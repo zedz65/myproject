39a3c43bb43459fff9ed5ebc35455fd4aa7002cb
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-20 01:03:51 +0000

    forms update

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, Booleanfield
+from wtforms import StringField, SubmitField
 from wtforms.validators import Datarequired, Length, ValidationError
 
 class CarForm(FlaskForm):
