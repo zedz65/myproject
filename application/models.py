6629adbe5ee79b36c8d451c2daa16e31b6991dfb
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-22 20:36:12 +0000

    models

--- application/models.py
+++ application/models.py
@@ -6,8 +6,8 @@ class Car(db.Model):
     model = db.Column(db.String(30), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     reg = db.Column(db.String(7), nullable=False, unique=True)
-    parts = db.relationship('Part')
-    #parts = db.relationship('Part', backref='cars_id', lazy=True)
+    #parts = db.relationship('Part')
+    parts = db.relationship('Part', backref='parts', lazy=True)
 
     def __repr__(self):
         return ''.join([
