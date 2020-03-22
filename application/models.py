c1ce8915e0edb4931853322ade2e4927b9997e18
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-22 16:39:49 +0000

    routes trying to add parts to db link

--- application/models.py
+++ application/models.py
@@ -6,7 +6,8 @@ class Car(db.Model):
     model = db.Column(db.String(30), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     reg = db.Column(db.String(7), nullable=False, unique=True)
-    parts = db.relationship('Part', back_populates='car.car_id')
+    #parts = db.relationship('Part', back_populates='car.car_id')
+    part = db.relationship('Part', backref='cars_id', lazy=True)
 
     def __repr__(self):
         return ''.join([
@@ -21,7 +22,7 @@ class Part(db.Model):
     part_name = db.Column(db.String(500), nullable=False)
     part_desc = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    #cars_id = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=False)
+    cars_id = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=False)
     #car = db.relationship('Car', back_populates='parts')
 
 
