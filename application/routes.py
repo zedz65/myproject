b9c9c5cd52034649522b5c197d7a788718b0abc2
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-22 18:26:47 +0000

    foreign null

--- application/routes.py
+++ application/routes.py
@@ -47,7 +47,7 @@ def parts():
             
         )
 
-        db.session.cars_id.append.part(Car())
+        db.session.append.cars_id.part(Car())
         db.session.add(partData)
         db.session.commit()
         return redirect(url_for('home'))
