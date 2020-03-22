c019dc0b49f19b7e741a1bf5187402d2541857e1
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-03-22 18:42:34 +0000

    foreign null

--- application/models.py
+++ application/models.py
@@ -22,8 +22,8 @@ class Part(db.Model):
     part_name = db.Column(db.String(500), nullable=False)
     part_desc = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    cars_id = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=False)
-    car = db.relationship('Car', back_populates='parts')
+    cars_id = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=True)
+    #car = db.relationship('Car', back_populates='parts')
 
 
     def __repr__(self):
